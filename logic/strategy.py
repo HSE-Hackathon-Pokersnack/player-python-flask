7ac0832af4904e39bf6256e7173de3c1a18b43ec
Leopold Stenger <lestit01@hs-esslingen.de>
2023-11-16 21:57:14 +0100

    update strat0

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,31 +1,55 @@
 from models.bet import Bet
 from models.table import Table
+from models.card import Card
+from typing import List
 
 
-def Givebet():
-    player = Table.players[3]
+def check_flush(card1, card2, comm_cards) -> bool:
+    all_cards = [card1, card2]
+    all_cards.append(comm_cards)
+    counter: int = 0
+
+    for i in range(1, len(all_cards)):
+        if all_cards[i - 1].suit == all_cards[i].suit:
+            counter += 1
+        if counter == 5:
+            return True
+
+    return False
+
+
+# Philip: "Reimt sich ja: 'Ist schon Witzig mit 70 :D !!!'"
+def strat_0(table: Table) -> int:
+    bet = 0
+    if table.minimumBet <= 70:
+        bet = 70
+    else:
+        bet = table.minimumBet
+
+    if table.minimumBet > 200:
+        bet = 0
+    return bet
+
+
+def strat_1(table: Table) -> int:
+    player = table.players[2]
     multiplier = 1
     matches = 0
     matchesInCommunityCards = 0
-    card1 = player["cards"][0]
-    card2 = player["cards"][1]
-    commmunityCards = Table["communityCards"]
-    commmunityCard1 = commmunityCards[0]
-    commmunityCard2 = commmunityCards[1]
-    commmunityCard3 = commmunityCards[2]
+    card1 = player.cards[0]
+    card2 = player.cards[1]
+    cCards = table.communityCards
 
-    if commmunityCard1 == commmunityCard2:
-        matchesInCommunityCards += 1
-    if commmunityCard2 == commmunityCard3:
-        matchesInCommunityCards += 1
-    if commmunityCard1 == commmunityCard3:
-        matchesInCommunityCards += 1
+    for i in range(len(cCards)):
+        for i2 in range(len(cCards)):
+            if cCards[i].rank == cCards[i2].rank:
+                matchesInCommunityCards += 1
 
-    if card1["rank"] == card2["rank"]:
+    if card1.rank == card2.rank:
         matches += 1
 
-    for card in commmunityCards:
-        if card["rank"] == card1["rank"] or card["rank"] == card2["rank"]:
+    for card in cCards:
+        if card.rank == card1.rank or card.rank == card2.rank:
             matches += 1
 
     match matches:
@@ -46,8 +70,20 @@ def Givebet():
             multiplier = 50
         case _:
             multiplier = 1
-    return 1
+
+    if check_flush(card1, card2, cCards):
+        multiplier = 50
+
+    return multiplier * table.minimumBet
+
+
+def checkStraight(card1: Card, card2: Card, comm_Cards: List[Card]):
+    result = 0
+    return result
 
 
 def decide(table: Table) -> Bet:
-    return Bet(70)
+    # call first strategy
+    # strat1_bet = strat_1(table)
+    chips = strat_0(table)
+    return Bet(chips)
--- models/table.py
+++ models/table.py
@@ -15,13 +15,15 @@ class Table:
     currentDealer: int
 
     def __init__(self, table: dict):
-        self.communityCards = table['communityCards']
-        for player in table['players']:
+        # self.communityCards = table["communityCards"]
+        for card in table["communityCards"]:
+            self.communityCards.append(Card(card))
+        for player in table["players"]:
             self.players.append(Player(player))
-        self.round = table['round']
-        self.smallBlind = table['smallBlind']
-        self.minimumBet = table['minimumBet']
-        self.minimumRaise = table['minimumRaise']
-        self.pot = table['pot']
-        self.activePlayer = table['activePlayer']
-        self.currentDealer = table['currentDealer']
+        self.round = table["round"]
+        self.smallBlind = table["smallBlind"]
+        self.minimumBet = table["minimumBet"]
+        self.minimumRaise = table["minimumRaise"]
+        self.pot = table["pot"]
+        self.activePlayer = table["activePlayer"]
+        self.currentDealer = table["currentDealer"]

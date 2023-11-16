f8d4a4afd5e87f71ad0c9f890bfdc3ee8f4c7856
Leopold Stenger <lestit01@hs-esslingen.de>
2023-11-16 20:47:50 +0100

    revert strat and fix bet 70

--- logic/strategy.py
+++ logic/strategy.py
@@ -2,14 +2,24 @@ from models.bet import Bet
 from models.table import Table
 
 
-def decide(table: Table) -> Bet:
-    # TODO: Add Poker Logic Here... :)
+def Givebet():
+    player = Table.players[3]
     multiplier = 1
     matches = 0
-    player = Table.players[Table["activePlayer"]]
+    matchesInCommunityCards = 0
     card1 = player["cards"][0]
     card2 = player["cards"][1]
     commmunityCards = Table["communityCards"]
+    commmunityCard1 = commmunityCards[0]
+    commmunityCard2 = commmunityCards[1]
+    commmunityCard3 = commmunityCards[2]
+
+    if commmunityCard1 == commmunityCard2:
+        matchesInCommunityCards += 1
+    if commmunityCard2 == commmunityCard3:
+        matchesInCommunityCards += 1
+    if commmunityCard1 == commmunityCard3:
+        matchesInCommunityCards += 1
 
     if card1["rank"] == card2["rank"]:
         matches += 1
@@ -19,11 +29,25 @@ def decide(table: Table) -> Bet:
             matches += 1
 
     match matches:
+        # pair
         case 1:
             multiplier = 2
+            # Full House
+            if matchesInCommunityCards == 2:
+                multiplier = 10
+        # three of a kind
         case 2:
             multiplier = 5
+            # Full House
+            if matchesInCommunityCards >= 1:
+                multiplier = 10
+        # four of a kind
         case 3:
             multiplier = 50
+        case _:
+            multiplier = 1
+    return 1
 
-    return Bet(multiplier * Table["minimumBet"])
+
+def decide(table: Table) -> Bet:
+    return Bet(70)
